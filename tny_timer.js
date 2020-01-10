0e646331c24ff5b38efa7c73fe088c35663755b3
Abraham Rivera-Garcia <arivera178@west-mec.org>
2020-01-10 15:14:54 -0700

    Finished it all, too lazy to bother you with so many commits.

--- tny_timer.js
+++ tny_timer.js
@@ -10,6 +10,29 @@
 
 */
 
+showClock();
+setInterval("showClock()", 1000);
+
+function showClock(){
+   var thisDay = new Date();
+   var localDate = thisDay.toLocaleDateString();
+   var localTime = thisDay.toLocaleTimeString();
+   document.getElementById("currentTime").innerHTML = "<span>"+localDate+"</span><span>"+localTime+"</span>";
+   var j4Date = nextJuly4(thisDay);
+   j4Date.setHours("21");
+
+   var days = (j4Date - thisDay) /(1000*60*60*24);
+   var hrs = (days - Math.floor(days))*24;
+   var mins = (hrs - Math.floor(hrs))*60;
+   var secs = (mins - Math.floor(mins))*60;
+
+
+
+   document.getElementById("dLeft").textContent = Math.floor(days);
+   document.getElementById("hLeft").textContent = Math.floor(hrs);
+   document.getElementById("sLeft").textContent = Math.floor(secs);
+}
+
 
 function nextJuly4(currentDate) {
    var cYear = currentDate.getFullYear();
@@ -18,3 +41,4 @@ function nextJuly4(currentDate) {
    if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
    return jDate;
 }
+
